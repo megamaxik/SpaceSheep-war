bb6ea46e911d29569132edae509e6dc2e289015a
Max <maxmailspb@mail.ru>
2023-01-05 14:46:14 +0300

    добавил звуки и хп

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import pygame_menu
 from os import path
 img_dir = path.join(path.dirname(__file__), 'images')
+snd_dir = path.join(path.dirname(__file__), 'sound')
 WIDTH = 1400
 HEIGHT = 1000
 FPS = 60
@@ -28,8 +29,28 @@ bg = pygame.transform.scale(bg, (1400, 1000))
 player_img = pygame.image.load(path.join(img_dir, "SpaceSheep.png")).convert()
 enemy_img = pygame.image.load(path.join(img_dir, "enemy.png")).convert()
 bullet_img = pygame.image.load(path.join(img_dir, "shot.png")).convert()
-global dif
-dif = 1
+expl_sounds = []
+shoot_sound = pygame.mixer.Sound(path.join(snd_dir, 'shoot.mp3'))
+expl_sounds.append(pygame.mixer.Sound(path.join(snd_dir, 'expl1.mp3')))
+expl_sounds.append(pygame.mixer.Sound(path.join(snd_dir, 'expl2.wav')))
+expl_sounds.append(pygame.mixer.Sound(path.join(snd_dir, 'expl3.mp3')))
+
+def draw_hp_bar(surf, x, y, pct):
+    if pct < 0:
+        pct = 0
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 10
+    fill = (pct / 100) * BAR_LENGTH
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
+    pygame.draw.rect(surf, GREEN, fill_rect)
+    pygame.draw.rect(surf, WHITE, outline_rect, 2)
+
+
+def newenemy():
+    m = enemy()
+    all_sprites.add(m)
+    mobs.add(m)
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -62,6 +83,8 @@ class enemy(pygame.sprite.Sprite):
             self.rect.x = WIDTH - 50
             self.speedx = -3
 
+    def dead(self):
+        self.kill()
 
 
 class Player(pygame.sprite.Sprite):
@@ -74,6 +97,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.bottom = HEIGHT / 2
         self.speedy = 0
         self.speedx = 0
+        self.hp = 100
 
     def update(self):
         self.speedy = 0
@@ -108,19 +132,20 @@ class Player(pygame.sprite.Sprite):
 
 
 def set_sound(value, sound):
+    global issound
+    issound = sound
     pass
 
 def set_difficulty(value, difficulty):
+
     pass
 
 
 
 def start_the_game():
-
-
     player = Player()
     all_sprites.add(player)
-    for i in range(8 * dif):
+    for i in range(8):
         m = enemy()
         all_sprites.add(m)
         mobs.add(m)
@@ -133,22 +158,29 @@ def start_the_game():
                 running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
+                    shoot_sound.play()
                     player.shoot()
         all_sprites.update()
         hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
         for hit in hits:
-            m = enemy()
-            all_sprites.add(m)
-            mobs.add(m)
+            expl_sounds[0].play()
 
-        hits = pygame.sprite.spritecollide(player, mobs, False)
-        if hits:
-            player.dead()
+            newenemy()
 
-            running = False
-
+        hits = pygame.sprite.spritecollide(player, mobs, True, pygame.sprite.collide_circle)
+        for hit in hits:
+            player.hp -= 10
+            expl_sounds[2].play()
+            newenemy()
+            if player.hp <= 0:
+                expl_sounds[1].play()
+                all_sprites.remove(player)
+                for i in range(8):
+                    all_sprites.remove(m)
+                running = False
         screen.blit(bg,(0, 0))
         all_sprites.draw(screen)
+        draw_hp_bar(screen, 5, 5, player.hp)
         pygame.display.flip()
 
 class MainScreen():
@@ -158,7 +190,7 @@ class MainScreen():
     enemy_img = pygame.image.load(path.join(img_dir, "enemy.png")).convert()
     bullet_img = pygame.image.load(path.join(img_dir, "shot.png")).convert()
     menu = pygame_menu.Menu('   Space Sheep War', 400, 400, theme=pygame_menu.themes.THEME_DARK)
-    menu.add.selector("Sound :", [("on", 1), ("off", 2)], onchange=set_sound)
+    menu.add.selector("Sound :", [("on", True), ("off", False)], onchange=set_sound)
     menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
     menu.add.button('Play', start_the_game)
     menu.add.button('Quit', pygame_menu.events.EXIT)
